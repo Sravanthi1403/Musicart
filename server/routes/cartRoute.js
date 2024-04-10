5695c571387ed43c101f1b63c5a446a074775529
A Sravanthi <rsravanthi555@gmail.com>
2024-04-10 21:37:17 +0530

    removed comments

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const cartControllers = require('../controllers/cartController');
+const isAuthenticated = require('../middlewares/authMiddleware');
 
 
 router.route('/addProductToCart').post(cartControllers.addProductToCart);
-router.route('/getCartProducts').get(cartControllers.getCartProducts);
+router.route('/getCartProducts').get(isAuthenticated, cartControllers.getCartProducts);
 router.route('/updateQuantity').put(cartControllers.updateCartItemQuantity);
 router.route('/deleteAllItems').delete(cartControllers.deleteAllItems);
 
