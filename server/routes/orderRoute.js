f7ab7db10283bea08be5327b050f4ad96ba02061
A Sravanthi <rsravanthi555@gmail.com>
2025-12-28 17:01:38 +0530

    order route changed

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const orderControllers = require('../controllers/orderController');
 const isAuthenticated = require('../middlewares/authMiddleware');
 
-router.route('/placeOrder').post(orderControllers.placeOrder);
+router.route('/placeOrder').post(isAuthenticated, orderControllers.placeOrder);
 router.route('/myOrders').get(isAuthenticated, orderControllers.getMyOrders);
 
 module.exports = router;
